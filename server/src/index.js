445e4498cfbc0034b5d9f5b8663ed564734a8c3a
paresiqbal <thecrowdel@gmail.com>
2023-06-16 13:52:42 +0700

    First setup complete

--- server/src/index.js
+++ server/src/index.js
@@ -3,5 +3,10 @@ import express from "express";
 // Express App
 const app = express();
 
+// Route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
 // Listen to port
 app.listen(3001, () => console.log("Server Running ..."));
