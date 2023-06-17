a9b5577b1ca01294a4b56fcd51d24788209d1511
paresiqbal <thecrowdel@gmail.com>
2023-06-17 22:24:00 +0700

    Add emote

--- server/controllers/WorkoutsController.js
+++ server/controllers/WorkoutsController.js
@@ -22,7 +22,7 @@ const getIndividualWorkout = async (req, res) => {
 
   const workout = await WorkoutsModel.findById(id);
   if (!workout) {
-    return res.status(404).json({ error: "No such workout" });
+    return res.status(404).json({ error: "No such workout 😢" });
   }
 
   res.status(200).json(workout);
