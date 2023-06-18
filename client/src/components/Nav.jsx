bd5dfb0532c078f115f8c065cc0d5086c0e6dac6
paresiqbal <thecrowdel@gmail.com>
2023-06-18 15:07:15 +0700

    asdasd

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,11 +3,8 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="flex p-4">
-      <Link
-        to={"/"}
-        className="border-2 py-2 px-10 rounded-md  font-bold hover:border-green-500"
-      >
+    <div className="flex p-6 shadow-md ">
+      <Link to={"/"} className="font-bold text-xl">
         Workout
       </Link>
     </div>
