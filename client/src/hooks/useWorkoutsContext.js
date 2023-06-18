81f4df2a2dd35ca06c9667348f2505311d5dbe95
paresiqbal <thecrowdel@gmail.com>
2023-06-18 16:42:50 +0700

    add useWorkoutsContext and it's so confusing

---
+++ client/src/hooks/useWorkoutsContext.js
@@ -0,0 +1,15 @@
+// Context
+import { WorkoutContext } from "../context/WorkoutContext";
+import { useContext } from "react";
+
+export const useWorkoutsContext = () => {
+  const context = useContext(WorkoutContext);
+
+  if (!context) {
+    throw Error(
+      "useWorkoutContext must be use indise the WorkoutContextProvider"
+    );
+  }
+
+  return context;
+};
