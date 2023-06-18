5ddcefd4d481f639fa8e221ab9bdbaa36a41b241
paresiqbal <thecrowdel@gmail.com>
2023-06-18 14:11:20 +0700

    Add tailwind

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="p-10">
+      <h1 className="text-center text-3xl">Hello World</h1>
     </div>
   );
 }
