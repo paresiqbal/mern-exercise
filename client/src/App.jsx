4f3cb589323579cc3dcd524230d30069da777ce9
paresiqbal <thecrowdel@gmail.com>
2023-06-18 14:09:49 +0700

    Add dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,24 @@
 // React Router Dom
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Pages
 import MainLayout from "./layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
 import Error from "./pages/Error";
 
-// router setuo
+// router setup
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
     errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Dashboard />,
+        errorElement: <Error />,
+      },
+    ],
   },
 ]);
 
