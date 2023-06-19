71493ab260daac4acde2d293581db3c59420f6c3
paresiqbal <thecrowdel@gmail.com>
2023-06-19 22:21:18 +0700

    Create WorkoutReducer

--- client/src/context/WorkoutContext.jsx
+++ client/src/context/WorkoutContext.jsx
@@ -3,10 +3,17 @@ import { createContext, useReducer } from "react";
 
 export const WorkoutsContext = createContext();
 
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_WORKOUTS":
+      return { workouts: action.payload };
+    case "CREATE_WORKOUT":
+      return { workouts: [action.payload, ...state.workouts] };
+  }
+};
+
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
-
-  dispatch({ type: "SET_WORKOUT" });
 
   return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
 };
