b6393f04a4826b9df0c0ec05ba3c6a8c47772668
paresiqbal <thecrowdel@gmail.com>
2023-06-19 22:17:15 +0700

    Trying dispatch

--- client/src/context/WorkoutContext.jsx
+++ client/src/context/WorkoutContext.jsx
@@ -6,5 +6,7 @@ export const WorkoutsContext = createContext();
 export const WorkoutsContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
 
+  dispatch({ type: "SET_WORKOUT" });
+
   return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
 };
