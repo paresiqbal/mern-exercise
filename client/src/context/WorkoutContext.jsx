3f2c62c461b5ecc65a118f9d82f84cb3e5474150
paresiqbal <thecrowdel@gmail.com>
2023-06-19 22:16:48 +0700

    Add template reducer

--- client/src/context/WorkoutContext.jsx
+++ client/src/context/WorkoutContext.jsx
@@ -4,7 +4,7 @@ import { createContext, useReducer } from "react";
 export const WorkoutsContext = createContext();
 
 export const WorkoutsContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer();
+  const [state, dispatch] = useReducer(workoutsReducer, { workouts: null });
 
   return <WorkoutsContext.Provider>{children}</WorkoutsContext.Provider>;
 };
