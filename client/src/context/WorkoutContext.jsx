a5af4c65d65ec30b6cef8f777c77ec04462fbbc1
paresiqbal <thecrowdel@gmail.com>
2023-06-19 22:11:04 +0700

    Trying new context

--- client/src/context/WorkoutContext.jsx
+++ client/src/context/WorkoutContext.jsx
@@ -1,31 +1,8 @@
 // React
-import { createContext, useReducer } from "react";
+import { createContext } from "react";
 
-export const WorkoutContext = createContext();
+export const WorkoutsContext = createContext();
 
-export const workoutsReducer = (state, action) => {
-  switch (action.type) {
-    case "SET_WORKOUTS":
-      return {
-        workouts: action.payload,
-      };
-    case "CREATE_WORKOUT":
-      return {
-        workouts: [action.payload, ...state.workouts],
-      };
-    default:
-      return state;
-  }
-};
-
-export const WorkoutContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(workoutsReducer, {
-    workouts: null,
-  });
-
-  return (
-    <WorkoutContext.Provider value={{ ...state, dispatch }}>
-      {children}
-    </WorkoutContext.Provider>
-  );
+export const WorkoutsContextProvider = () => {
+  return <WorkoutsContext.Provider></WorkoutsContext.Provider>;
 };
