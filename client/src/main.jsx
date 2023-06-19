f5dd1b8668e86714c2c9630205d8ac9be595f1ab
paresiqbal <thecrowdel@gmail.com>
2023-06-19 22:11:35 +0700

    Remove previeous context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 
 // Context
-import { WorkoutContextProvider } from "./context/WorkoutContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
