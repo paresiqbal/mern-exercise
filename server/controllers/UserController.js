ff31f10edb6e8ddaff1ffedfc9998879f889e385
paresiqbal <thecrowdel@gmail.com>
2023-06-20 15:31:48 +0700

    Signup failed

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,3 +1,5 @@
+import { UserModel } from "../models/UserModel.js";
+
 // login user
 const loginUser = async (req, res) => {
   res.json({ message: "Login success" });
@@ -5,7 +7,15 @@ const loginUser = async (req, res) => {
 
 // signup user
 const signUser = async (req, res) => {
-  res.json({ message: "Signup success" });
+  const { email, password } = req.body;
+  try {
+    const user = await UserModel.signup(email, password);
+
+    // response
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 export { loginUser, signUser };
--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -10,7 +10,7 @@ const UserSchema = new Schema({
 });
 
 // static signup method
-UserSchema.static.signup = async (email, password) => {
+UserSchema.static.signup = async function (email, password) {
   const emailExists = await this.findOne({ email });
 
   if (emailExists) {
