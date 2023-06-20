d2a542f8e308a5b82461e29f280154ac6a2d8ae7
paresiqbal <thecrowdel@gmail.com>
2023-06-20 15:22:13 +0700

    Add bcrypt

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,5 +1,6 @@
 // Library
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -7,5 +8,23 @@ const UserSchema = new Schema({
   email: { type: String, require: true, unique: true },
   password: { type: String, require: true },
 });
+
+// static signup method
+UserSchema.static.signup = async (email, password) => {
+  const emailExists = await this.findOne({ email });
+
+  if (emailExists) {
+    throw Error("Email already in use");
+  }
+
+  // hash user password
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  // save to db
+  const user = await this.create({ email, password: hash });
+
+  return user;
+};
 
 export const UserModel = mongoose.model("user", UserSchema);
