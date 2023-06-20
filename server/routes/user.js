252b6050e06a6cef91b6c3d6750231708b2ed942
paresiqbal <thecrowdel@gmail.com>
2023-06-20 14:02:35 +0700

    setup user route

---
+++ server/routes/user.js
@@ -0,0 +1,13 @@
+// Library
+import express from "express";
+
+// router setup
+const router = express.Router();
+
+// login route
+router.post("/login", () => {});
+
+// signup route
+router.post("/signup", () => {});
+
+export { router as userRouter };
