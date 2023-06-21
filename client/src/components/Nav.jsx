cfcb012bc4cd5cf9533049cd4829d11ca2aeae38
paresiqbal <thecrowdel@gmail.com>
2023-06-21 10:08:01 +0700

    Add styling

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -19,15 +19,29 @@ export default function Nav() {
       </Link>
       <section className="flex gap-4">
         {!user && (
-          <div className="flex gap-4">
-            <Link to={"/signup"}>Sign Up</Link>
-            <Link to={"/login"}>login</Link>
+          <div className="flex items-center gap-4">
+            <Link
+              to={"/signup"}
+              className="text-white bg-teal-500 py-2 px-4 rounded-md active:bg-teal-600"
+            >
+              Sign Up
+            </Link>
+            <Link to={"/login"} className="font-bold">
+              Login
+            </Link>
           </div>
         )}
         {user && (
-          <div>
-            <span>{user.email}</span>
-            <button onClick={handleLogout}>Logout</button>
+          <div className="flex gap-4">
+            <span className="text-sm flex items-center justify-center">
+              {user.email}
+            </span>
+            <button
+              onClick={handleLogout}
+              className="outline outline-1 outline-red-500 hover:outline-2 active:bg-red-500 py-2 px-4 rounded-md"
+            >
+              Logout
+            </button>
           </div>
         )}
       </section>
