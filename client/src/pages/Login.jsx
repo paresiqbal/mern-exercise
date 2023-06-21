27cd836187b3e1c8af155ec6d6ab382a5b191720
paresiqbal <thecrowdel@gmail.com>
2023-06-21 09:50:57 +0700

    Add login form styling

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,21 +12,39 @@ export default function Login() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h1>Login</h1>
-      <section>
-        <label htmlFor="email">Email:</label>
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col items-center p-10 gap-6"
+    >
+      <h1 className="text-3xl font-bold">Login</h1>
+      <section className="flex flex-col w-full gap-2">
+        <label htmlFor="email" className="text-xl">
+          Email:
+        </label>
         <input
           type="email"
           name="email"
           id="email"
           onChange={(e) => setEmail(e.target.value)}
           value={email}
+          className="p-2 rounded-md text-gray-600"
         />
-        <label htmlFor="password">Password:</label>
-        <input type="password" name="password" id="password" />
+        <label htmlFor="password" className="text-xl">
+          Password:
+        </label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          className="p-2 rounded-md text-gray-600"
+        />
       </section>
-      <button type="submit">Login</button>
+      <button
+        type="submit"
+        className="bg-teal-500 py-2 px-6 rounded-md font-bold text-white active:bg-teal-600"
+      >
+        Login
+      </button>
     </form>
   );
 }
